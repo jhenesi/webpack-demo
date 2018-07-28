411f82a3de95869539e8fe4f5509fe818cb276d4
Jesús Héctor Neyoy Siari <jneyoy@novutek.com>
2018-07-27 17:36:28 -0700

    Setting up initial css

---
+++ webpack.parts.js
@@ -0,0 +1,20 @@
+exports.devServer = ({ host, port } = {}) => ({
+    devServer: {
+        stats: "errors-only",
+        host,
+        port,
+        open: true,
+        overlay: true
+    }
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+    module: {
+        rules: [{
+            test: /\.css$/,
+            include,
+            exclude,
+            use: ["style-loader", "css-loader"]
+        }]
+    }
+});
